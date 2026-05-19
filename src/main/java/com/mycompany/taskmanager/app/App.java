61b80144c2687a777d20a8fe61041cad14395900
Willian Frota <willianfrotadev@gmail.com>
2026-05-19 13:54:21 -0300

    Tentando entender como a I.A fez para conseguir aprender

--- src/main/java/com/mycompany/taskmanager/app/App.java
+++ src/main/java/com/mycompany/taskmanager/app/App.java
@@ -6,6 +6,7 @@ package com.mycompany.taskmanager.app;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 /**
@@ -16,23 +17,30 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(
-            getClass().getResource("/com/mycompany/taskmanager/view/main.fxml")
-        );
+        FXMLLoader loader = new FXMLLoader( getClass().getResource("/com/mycompany/taskmanager/view/main.fxml"));
+        // cria o carregador FXMLLoader para ler esse arquivo e encontra o arquivo (.fxml)
+        // FXMLLoader é a classe que le o xml do FXML, cria componentes javafx; conecta controller,@FXML e onAction
+        // getResource serve para localizar o arquivo dentro do projeto
+        // getClass serve para pegar a classe atual do objeto (nesse caso com.mycompany.taskmanager.app.App) quando nao esta associado a um objeto ele usa a classe que voce esta editando
+        // url: Uniform Resource Locator
+        // nesse caso ele aponta para o caminho do FXML compilado, que nesse caso seria o caminho da classe atual
+        // entendi agora apartir da classe a atual, ela tem um metodo chamado getResource que determina onde esta o caminho do arquivo fxml
         
+        Parent root = loader.load();// diferente de usar o loader.load direto no scene é que da pra estilizar o root antes de carregar a janela
+        // usando o root da para reutilizar o codigo
 
-        Scene scene = new Scene(loader.load());
-        stage.setWidth(800);
-        stage.setHeight(600);
-        stage.setTitle("Task Manager");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(root, 900, 600);// width: largura = 900 e height: altura = 600
+        stage.setMinWidth(820);// setar a largura minima
+        stage.setMinHeight(520);// setar a altura minima
+        stage.setTitle("Task Manager");// ja colocar um titulo na janela
+        stage.setScene(scene);// carregar a scene
+        stage.show();// mostrar o stage, uma traducao ao pe da letra seria estagio atual da tela
     
     
     
     }
     public static void main(String[] args) {
-        launch(args);
+        launch(args);// isso inicia a aplicacao javafx, usado para chamar o metodo start criado la em cima
     }
     
     
--- src/main/java/com/mycompany/taskmanager/controller/MainController.java
+++ src/main/java/com/mycompany/taskmanager/controller/MainController.java
@@ -6,36 +6,66 @@ package com.mycompany.taskmanager.controller;
 
 import com.mycompany.taskmanager.model.Tarefa;
 import com.mycompany.taskmanager.model.TarefaPrioritaria;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 /**
  *
  * @author willianfrota
  */
 public class MainController {
-    @FXML
-    private TextField campoTitulo;
-    @FXML
-    private TextArea campoDescricao;
     
-    private ListaDeTarefas<Tarefa> tarefas = new ListaDeTarefas<>();
-    private TaskManager gerenciador = new TaskManager();
     @FXML
-    private CheckBox checkPrioridade;
+    private TextField campoTitulo;//lugar para digitar o titulo da tarefa a ser adicionada ou editada
+    
+    @FXML
+    private TextArea campoDescricao;//lugar para digitar a descricao da tarefa
+    
     @FXML 
-    private ListView<Tarefa> listaTarefas = new ListView<>();
+    private CheckBox checkPrioritaria;// uma checkbox para determinar se a tarefa a ser editada ou criada é tarefa normal ou se é tarefa prioritaria
+    
+    @FXML
+    private VBox criaTarefa;// area que vai aparecer na hora de editar ou criar uma tarefa é nela que vai ter todos os componentes acima
+    
+    @FXML 
+    private Label labelCriaTarefa;// nele vai dizer se voce esta editando uma tarefa ou adicionando uma tarefa
+    
+    @FXML 
+    private ListView<Tarefa> listaTarefas;// lista de tarefas que aparecera na tela, ela é totalmente independente da listas do backend por isso é preciso repassar as listas para ela
+    
+    @FXML
+    private Label labelCriaGeral;// todo o texto que tera em cada espaco de tarefa na lista de tarefas
+    
+    private final ListaDeTarefas<TarefaPrioritaria> tarefasPrio = new ListaDeTarefas<>();// guarda no backend as tarefas prioritarias
+    private final ListaDeTarefas<Tarefa> tarefas = new ListaDeTarefas<>();// guarda no back end as tarefas normais
+    private final TaskManager gerenciador = new TaskManager();// gerencia as duas listas de cima
+    private final ObservableList<Tarefa> tarefasNaTela = FXCollections.observableArrayList();// essa é a lista que alimenta o ListView listaTarefas
+    
+    private Tarefa editaTarefa;// guarda uma tarefa que vai ser editada no momento
+    
+    @FXML
+    private void initialize(){ // esse metodo ele é ativado automaticamente quando a tela FXML termina de carregar, toda vez que voce adicionar, remover ou editar ele vai ativar esse metodo
+        
+        listaTarefas.setItems(tarefasNaTela);// serve para mostrar os dados de outra lista, detalhe toda vez que
+        listaTarefas.setCellFactory(lista -> new ListCell<>(){
+         
+            
+        
+        });
+        
+    }
+    
     @FXML
     private void adicionar() {
-        formNovaTarefa.setVisible(true);
-        formNovaTarefa.setManaged(true);
-
-        campoTitulo.clear();
-        campoDescricao.clear();
-        campoTitulo.requestFocus();
+        
         
     }
 
