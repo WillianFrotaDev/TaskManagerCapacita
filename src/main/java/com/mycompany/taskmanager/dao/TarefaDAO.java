3e1ae4f6a9922e5c34f9dec5bdfd339a26b88d33
Willian Frota <willianfrotadev@gmail.com>
2026-05-21 20:47:54 -0300

    tentando interligar banco de dados com o MainController

--- src/main/java/com/mycompany/taskmanager/dao/TarefaDAO.java
+++ src/main/java/com/mycompany/taskmanager/dao/TarefaDAO.java
@@ -78,7 +78,9 @@ public class TarefaDAO {
            stm.setBoolean(3, tarefa.getConcluida());
            stm.setBoolean(4, tarefa.getPrioridade());
            
-           stm.executeUpdate();// ele é usado para operacoes em sql, usado nas operacoes INSERT, UPDATE e DELETE
+           //executeUpdate: altera dados
+           //executeQuery: busca dados
+           stm.executeUpdate();// ele é usado para operacoes em sql, usado nas operacoes INSERT, UPDATE e DELETE, 
            //tambem usado na criacao de tabelas (CREATE TABLE) e em alteracoes estruturais (ALTER TABLE)
             
            conexao.close();
@@ -89,34 +91,36 @@ public class TarefaDAO {
 
     }
     
-    public void buscarPorId(int id) throws SQLException{
+    public Tarefa buscarPorId(int id) throws SQLException{
         
         String sql = "SELECT * FROM tarefas WHERE id = ?";
         Connection conexao = ConexaoFactory.conectar();
         PreparedStatement state = conexao.prepareStatement(sql);
         
-        state.setInt(1, id);
-        ResultSet resultadoConsulta = state.executeQuery();
+        try{
+            state.setInt(1, id);
+            ResultSet resultadoConsulta = state.executeQuery();// executeQuery serve para consultar os dados
         
-        if (resulta.next()) {
+            if (resultadoConsulta.next()) {
 
-            String titulo = rs.getString("titulo");
-            String descricao = rs.getString("descricao");
-            boolean prioritaria = rs.getBoolean("prioritaria");
+                // esses parametros desses metodos, é a categoria la da chave primaria
+                String titulo = resultadoConsulta.getString("titulo");
+                String descricao = resultadoConsulta.getString("descricao");
+                boolean prioritaria = resultadoConsulta.getBoolean("prioritaria");
 
-            Tarefa tarefa;
+                
+                Tarefa tarefa;
 
-            if (prioritaria) {
-                tarefa = new TarefaPrioritaria(titulo, descricao);
-            } else {
-                tarefa = new Tarefa(titulo, descricao);
+                if (prioritaria) {
+                    tarefa = new TarefaPrioritaria(titulo, descricao);
+                } else {
+                    tarefa = new Tarefa(titulo, descricao);
+                }
+
+                tarefa.setId(id);
+
+                return tarefa;
             }
-
-            tarefa.setId(idBusca);
-
-            return tarefa;
-        }
-
     } catch (SQLException e) {
         e.printStackTrace();
     }
@@ -126,9 +130,70 @@ public class TarefaDAO {
         
     }
     
-    public void editar(int id){}
+    public void editar(Tarefa tarefa){
+        
+        // vou usar junto com buscarPorId
+        String sql = """
+        UPDATE tarefas
+        SET titulo = ?, descricao = ?, concluida = ?, prioritaria = ?
+        WHERE id = ?""";
+
+        try (
+            Connection conexao = ConexaoFactory.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql)
+        ) {
+
+            stmt.setString(1, tarefa.getTitulo());
+            stmt.setString(2, tarefa.getDescricao());
+            stmt.setBoolean(3, tarefa.getConcluida());
+
+            // verifica se eh prioritaria, para ja atribuir ao comando que sera executado
+            stmt.setBoolean(4, tarefa instanceof TarefaPrioritaria);
+
+            stmt.setInt(5, tarefa.getId());
+
+            int linhasAfetadas = stmt.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                System.out.println("Tarefa editada com sucesso!");
+            } else {
+                System.out.println("Nenhuma tarefa encontrada.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
     
-    public void remover(int id){}
+    public void remover(int id) {
+
+        //vou primeiro usar o metodo buscarPorId para depois pegar e executar esse
+        String sql = "DELETE FROM tarefas WHERE id = ?";
+
+        try (// try with resources para fechar automaticamente o connection e o prepared statement
+            Connection conexao = ConexaoFactory.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql)
+        ) {
+
+            stmt.setInt(1, id);// esse metodo serve para setar o valor "?"
+
+            int linhasAfetadas = stmt.executeUpdate();// quando se altera dados é executeUpdate, é
+            // o executeUpdate sempre retorna o numero de linhas afetadas pela alteracao que voce fez no banco de dados
+
+            if (linhasAfetadas > 0) {
+                System.out.println("Tarefa removida com sucesso!");
+                
+            } else {// quando nao da certo de alterar algo no banco de dados, as linhas afetadas sao zero!
+                
+                System.out.println("Nenhuma tarefa encontrada.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+    }
     
     
 }
